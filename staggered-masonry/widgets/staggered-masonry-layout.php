34d8a93d4a8817f9953ecbc0437bd764add28363
jamesdev500 <james@500designs.com>
2024-03-22 06:01:54 +0800

    fix: missing image alts

--- staggered-masonry/widgets/staggered-masonry-layout.php
+++ staggered-masonry/widgets/staggered-masonry-layout.php
@@ -2,9 +2,10 @@
 // staggered-masonry-layout.php
 
 function render_staggered_masonry($gallery, $heading_text) {
+
 ?>
 
-    <h2><?= esc_html($heading_text); ?></h2>
+    <h3><?= esc_html($heading_text); ?></h3>
 
     <div id="staggeredMasonry" class="section is--grid">
         <div class="grid_wrapper-contain">
@@ -16,10 +17,13 @@ function render_staggered_masonry($gallery, $heading_text) {
                     <div role="list" class="grid_list">
                         <?php for ($j = 0; $j < 4 && $i + $j < count($gallery); $j++) :
                             $image = $gallery[$i + $j];
-                            $alt_text = isset($image['alt']) ? $image['alt'] : '';
+                            $alt_text = get_post_meta($image['id'], '_wp_attachment_image_alt', true);
+                            if (empty($alt_text)) {
+                                $alt_text = get_the_title($image['id']);
+                            }
                         ?>
                             <div role="listitem" class="grid_item">
-                                <div class="grid_element"><img src="<?= esc_url($image['url']); ?>" loading="lazy" alt="<?= esc_attr($alt_text); ?>" class="grid_img" /></div>
+                                <div class="grid_element"><img src="<?= esc_url($image['url']); ?>" loading="lazy" alt="<?= esc_attr($alt_text); ?>" class="grid_img" data-name="<?= $image['id'] ?>" /></div>
                             </div>
                         <?php endfor; ?>
                     </div>
